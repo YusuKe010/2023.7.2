9c7531247ea13e99a3d07fc77bec4e688d37456c
YusuKe010 <yuusuke.k1717@gmail.com>
2023-07-02 12:09:52 +0900

    シーン作成

---
+++ Assets/yuusuke/bullet.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    /// <summary>
+    ///Å@íeÇÃÇ∆Ç‘ë¨Ç≥
+    /// </summary>
+    [SerializeField] float bulletspeed = 10.0f;
+    /// <summary>
+    /// íeÇÃè¡Ç¶ÇÈéûä‘
+    /// </summary>
+    [SerializeField] float bullettime = 5.0f;
+    // Start is called before the first frame update
+    public float Speed { get => bulletspeed; set => bulletspeed = value; }
+
+    void Start()
+    {
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        rb.velocity = Vector3.right * bulletspeed;
+
+        Destroy(this.gameObject, bullettime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
