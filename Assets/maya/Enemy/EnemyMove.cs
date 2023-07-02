a62e0d61d4751c669db3181ed6b07a3df7787865
MaeTakuto <maetakuto20001010@gmail.com>
2023-07-02 14:42:26 +0900

    ゴール追加

--- Assets/maya/Enemy/EnemyMove.cs
+++ Assets/maya/Enemy/EnemyMove.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemyMove : MonoBehaviour
@@ -7,6 +8,9 @@ public class EnemyMove : MonoBehaviour
     [SerializeField]
     private float _move_Power = -3;
     private Rigidbody2D _rb_enemy;
+
+    [SerializeField]
+    private int _enemy_hp = 1;
 
     // Start is called before the first frame update
     void Start()
@@ -17,16 +21,38 @@ public class EnemyMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // à⁄ìÆ
         Vector2 velocity = _rb_enemy.velocity;
         velocity.x = _move_Power;
         _rb_enemy.velocity = velocity;
+
+        // óéâ∫ÇµÇΩèÍçá
+        if(transform.position.y < -10)
+        {
+            Destroy(this.gameObject);
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        // ï«Ç‚ìGÇ…ìñÇΩÇ¡ÇΩèÍçá
         if(collision.gameObject.tag == "Wall" || collision.gameObject.tag == "Enemy")
         {
             _move_Power *= -1;
         }
+
+        // íeÇ∆ìñÇΩÇ¡ÇΩèÍçá
+        /*
+        if (collision.gameObject.tag == "Bullet")
+        {
+            _enemy_hp -= 1;
+
+            // HPÇ™0Ç…Ç»Ç¡ÇΩÇÁ
+            if(_enemy_hp <= 0)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+        */
     }
 }
